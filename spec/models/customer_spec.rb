3d77b056a869b4c9b4b3629c3815279cca3cf002
Gauri Joshi <gauri728@gmail.com>
2022-07-31 15:51:49 -0700

    user story2

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Customer, type: :model do
     it { should have_many(:customer_items)}
     it { should have_many(:items).through(:customer_items)}
   end
-end
+
+  describe "model methods" do 
+      it "total_price" do 
+        safeway = Supermarket.create!(name: "Safeway", location: "Fremont")
+        customer_1 = Customer.create!(name: "John Bolt", supermarket_id: safeway.id )
+        item_1 = Item.create!(name: "Cereal", price: 4)
+        item_2 = Item.create!(name: "Banana", price: 1)
+        customer_item1 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_1.id)
+        customer_item2 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_2.id)
+
+        expect(customer_1.total_price).to eq(5)
+      end
+    end
+  end
