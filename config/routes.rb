888f952b7d4e41a1767ca084f69166a68ad3362d
Gauri Joshi <gauri728@gmail.com>
2022-07-31 16:04:07 -0700

    supermarket show up until viewable link

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :customers, only: [:show]
+  resources :supermarkets, only: [:show]
 end
---
+++ spec/features/supermarkets/show_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Supermarket show page" do 
+   it "has the name of that supermarket and a link to view all of the items of that supermarket" do 
+      safeway = Supermarket.create!(name: "Safeway", location: "Fremont")
+      vons = Supermarket.create!(name: "Vons", location: "Irvine")
+
+      customer_1 = Customer.create!(name: "John Bolt", supermarket_id: safeway.id )
+      customer_2 = Customer.create!(name: "Amy Adams", supermarket_id: safeway.id )
+      customer_3 = Customer.create!(name: "Fred Rogers", supermarket_id: vons.id )
+
+      item_1 = Item.create!(name: "Cereal", price: 4)
+      item_2 = Item.create!(name: "Banana", price: 1)
+      item_3 = Item.create!(name: "Cooking oil", price: 5)
+
+      customer_item1 = CustomerItem.create!(customer_id: customer_1.id, item_id: item_1.id)
+      customer_item2 = CustomerItem.create!(customer_id: customer_2.id, item_id: item_2.id)
+      customer_item3 = CustomerItem.create!(customer_id: customer_2.id, item_id: item_3.id)
+
+      visit "/supermarkets/#{safeway.id}"
+
+      expect(page).to have_content("Safeway")
+      expect(page).to have_link("Items")
+   end
+end
+# As a visitor,
+# When I visit a supermarket show page,
+# I see the name of that supermarket,
+# And I see a link to view all of the items that the supermarket has
+# And when I click on the link,
+# I am taken to the supermarkets item index page,
+# And I can see a UNIQUE list of all the items that the supermarket has
